0895d002fba95a1ff901f1020647cfd479055ff3
ryan <ryanarnold.zoleta@gmail.com>
2026-09-12 00:55:44 +0800

    fix(mobile): bolden button text (#21)

--- apps/mobile/src/components/ui/button.tsx
+++ apps/mobile/src/components/ui/button.tsx
@@ -60,7 +60,7 @@ export function Button({
       {icon}
       <Text
         className={cn(
-          "shrink text-center font-sans",
+          "shrink text-center font-sans font-bold",
           variants[variant].text,
           sizes[size].text,
         )}
